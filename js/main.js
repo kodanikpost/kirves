affc6b45cd4d527f887086da5491f3da94d78abd
kodanikpost <83229733+kodanikpost@users.noreply.github.com>
2021-08-23 18:49:55 +0200

    adding video and squiggle gif

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
-const curriculum = document.getElementById('curriculum')
+var isPopupVideoOpened = false;
+
+function togglePopupVideo(state) {
+    document.getElementById('openvid').classList.toggle('active', state);
+    document.body.classList.toggle('active', state);
+    isPopupVideoOpened = state;
+}
+
+function openPopupVideo() {
+    togglePopupVideo(true)
+}
+
+function closePopupVideo() {
+    togglePopupVideo(false)
+}
+
+window.onload = function(e) {
+
+    document.addEventListener('click', function(e) {
+        var target = e.target;
+
+        if (target.id == 'open-popup-video') {
+            openPopupVideo();
+        } else if (isPopupVideoOpened) {
+            closePopupVideo();
+        }
+    })
+}
 
 function toggleCv () {
     curriculum.classList.toggle('active')
     document.body.classList.toggle('active')
 }
 
-const textElement = document.getElementById('text')
-const words = textElement.dataset.words.split(',')
-const typingSpeed = 100
-const pauseTime = 2000
-let count = 0
+var textElement = document.getElementById('text')
+var words = textElement.dataset.words.split(',')
+var typingSpeed = 100
+var pauseTime = 2000
+var count = 0
 
 function typeWord(word) {
-   let typeCount = 0
+   var typeCount = 0
 
-   const typeInterval = setInterval(() => {
+   var typeInterval = setInterval(() => {
        if (typeCount <= word.length) {
            textElement.innerHTML = word.slice(0, typeCount) + '<span class="cursor">|</span>'
            typeCount ++
@@ -24,7 +51,6 @@ function typeWord(word) {
                typeWord(nextWord())
            }, pauseTime)
        }
-
    }, typingSpeed)
 }
 
